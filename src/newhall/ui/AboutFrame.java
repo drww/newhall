865dd4a709d5c76f8a5fa27ffd9a464664b8889d
edwaltman <drww@users.noreply.github.com>
2010-10-04 07:17:47 +0000

    Removed redundant main() function.

--- src/newhall/ui/AboutFrame.java
+++ src/newhall/ui/AboutFrame.java
@@ -107,18 +107,6 @@ public class AboutFrame extends javax.swing.JFrame {
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
-
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AboutFrame().setVisible(true);
-            }
-        });
-    }
-
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JLabel jLabel1;
   private javax.swing.JLabel jLabel2;
