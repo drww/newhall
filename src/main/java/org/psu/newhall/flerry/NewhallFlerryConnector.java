c47a10fa40cdc168f371115f44044380d26f3d03
edwaltman <drww@users.noreply.github.com>
2011-03-17 18:37:01 +0000

    Fixed reflection ambiguity that hosed Flerry's ability to call the model.  New version 1.4.2.

--- src/main/java/org/psu/newhall/flerry/NewhallFlerryConnector.java
+++ src/main/java/org/psu/newhall/flerry/NewhallFlerryConnector.java
@@ -11,22 +11,22 @@ import org.psu.newhall.util.XMLStringResultsExporter;
 public class NewhallFlerryConnector {
 
   public static String runModel(String inputXmlFile, Boolean isMetric, Double waterHoldingCapacity, Double soilAirRel) {
-    return runModel(inputXmlFile, isMetric, waterHoldingCapacity, soilAirRel);
+    return runModelProper(inputXmlFile, isMetric.booleanValue(), waterHoldingCapacity.doubleValue(), soilAirRel.doubleValue());
   }
 
   public static String runModel(String inputXmlFile, Boolean isMetric, Integer waterHoldingCapacity, Integer soilAirRel) {
-    return runModel(inputXmlFile, isMetric, waterHoldingCapacity, soilAirRel);
+    return runModelProper(inputXmlFile, isMetric.booleanValue(), waterHoldingCapacity.doubleValue(), soilAirRel.doubleValue());
   }
 
   public static String runModel(String inputXmlFile, Boolean isMetric, Double waterHoldingCapacity, Integer soilAirRel) {
-    return runModel(inputXmlFile, isMetric, waterHoldingCapacity, soilAirRel);
+    return runModelProper(inputXmlFile, isMetric.booleanValue(), waterHoldingCapacity.doubleValue(), soilAirRel.doubleValue());
   }
 
   public static String runModel(String inputXmlFile, Boolean isMetric, Integer waterHoldingCapacity, Double soilAirRel) {
-    return runModel(inputXmlFile, isMetric, waterHoldingCapacity, soilAirRel);
+    return runModelProper(inputXmlFile, isMetric.booleanValue(), waterHoldingCapacity.doubleValue(), soilAirRel.doubleValue());
   }
 
-  public static String runModel(String inputXmlFile, Boolean isMetric, Number waterHoldingCapacity, Number soilAirRel) {
+  private static String runModelProper(String inputXmlFile, boolean isMetric, double waterHoldingCapacity, double soilAirRel) {
 
     XMLStringParser xsp = null;
 
@@ -39,8 +39,8 @@ public class NewhallFlerryConnector {
     }
 
     NewhallDataset dataset = xsp.getDataset();
-    dataset.getMetadata().setSoilAirOffset(soilAirRel.doubleValue());
-    NewhallResults results = BASICSimulationModel.runSimulation(dataset, waterHoldingCapacity.doubleValue());
+    dataset.getMetadata().setSoilAirOffset(soilAirRel);
+    NewhallResults results = BASICSimulationModel.runSimulation(dataset, waterHoldingCapacity);
 
     return XMLStringResultsExporter.export(results, dataset);
 
