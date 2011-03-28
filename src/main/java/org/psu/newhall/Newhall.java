b287547904a3059f6dfaa61c5ad12bd0086873ce
edwaltman <drww@users.noreply.github.com>
2011-03-28 06:18:35 +0000

    Updated to version 1.4.3.

--- src/main/java/org/psu/newhall/Newhall.java
+++ src/main/java/org/psu/newhall/Newhall.java
@@ -5,7 +5,7 @@ import org.psu.newhall.ui.DefaultNewhallFrame;
 
 public class Newhall {
 
-  public static String NSM_VERSION = "1.4.2";
+  public static String NSM_VERSION = "1.4.3";
 
   public static void main(String[] args) {
 
